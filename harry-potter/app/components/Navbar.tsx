febc7c2aac7e5e724d37b630b2211feca89cc770
cheronodaisy <jepchumbadaisy96@gmail.com>
2023-08-20 22:38:27 +0300

    finalized the navabr

--- harry-potter/app/components/Navbar.tsx
+++ harry-potter/app/components/Navbar.tsx
@@ -15,12 +15,27 @@ const navLinks: NavLink[] = [
 
 const Navbar: React.FC = () => {
     return (
-        <nav className="container mx-auto px-4">
-            <div className="flex items-center justify-between">
-                <h1 className="text-white text-2x1 font-semibold">
-                    Harry Potter<span className="text-yellow-500">App</span>
-                </h1>
+        <nav className="bg-gray-800 py-4">
+            <div className="container mx-auto px-4">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-white text-2x1 font-semibold">
+                        Harry Potter<span className="text-yellow-500">App</span>
+                    </h1>
+                    <ul className="flex items-center space-x-4 md:space-x-8">
+                        {navLinks.map((link, index) => (
+                            <li>
+                                <Link
+                                href={link.url}
+                                className="text-white hover: text-yellow-500 transition-colors duration-200 "
+                                >
+                                    {link.label}
+                            </Link>
+                            </li>
+                        ))}
+                    </ul>h
+                </div>
             </div>
+            
         </nav>
     )
 };
