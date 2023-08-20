8cb39f585bcd4697ec01c383280379373ec86a6e
cheronodaisy <jepchumbadaisy96@gmail.com>
2023-08-20 22:54:08 +0300

    created the character card component

--- harry-potter/app/components/CharacterCard.tsx
+++ harry-potter/app/components/CharacterCard.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { CharacterCard } from "../models/character";
 
-const CharacterCard = () => {
-    return <div>CharacterCard</div>
+interface CharacterCardProps {
+    characterCard: CharacterCard;
+}
+
+const CharacterCard: React.FC<CharacterCardProps> = ( { characterCard} ) => {
+    const { name, dateOfBirth } = characterCard;
+
+    return (
+        <div className="flex flex-col items-center p-8 mt-32 ">
+            <h2 className="text-2x1 mb-2">
+                {name}, {dateOfBirth}
+            </h2>
+        </div>
+    );
 };
 
 export default CharacterCard;
